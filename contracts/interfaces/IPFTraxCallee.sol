eb3acf5f65620cacd830ce982fd8faa33a07eb7e
PFTrax <94227482+pftrax@users.noreply.github.com>
2022-06-01 18:06:53 +0800

    Update IPFTraxCallee.sol

--- contracts/interfaces/IPFTraxCallee.sol
+++ contracts/interfaces/IPFTraxCallee.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
-interface IRimauCallee {
+interface IPFTXCallee {
     function rimauCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
