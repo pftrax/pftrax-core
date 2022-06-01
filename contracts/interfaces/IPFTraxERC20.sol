f981b784a65ba897581cd2f63de314831374169c
PFTrax <94227482+pftrax@users.noreply.github.com>
2022-06-01 18:07:03 +0800

    Update IPFTraxERC20.sol

--- contracts/interfaces/IPFTraxERC20.sol
+++ contracts/interfaces/IPFTraxERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
-interface IRimauERC20 {
+interface IPFTXERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
